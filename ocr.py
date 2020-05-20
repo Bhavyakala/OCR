612361427bfed2ab82a51f1518bf96615ee31288
Bhavya Kala <bhavya.kala10@gmail.com>
2020-05-20 22:53:40 +0530

    added space and spell correction

--- ocr.py
+++ ocr.py
@@ -6,10 +6,11 @@ import os
 import cv2
 import json
 
-path = 'D:/Coding_wo_cp/OCR/download.png'
+# path = 'D:/Coding_wo_cp/OCR/download.png'
 # path = 'D:/Coding_wo_cp/OCR/0325updated.task1train(626p)/X00016469612.jpg'
 # path = 'D:/Coding_wo_cp/ML_CHALLENGE_HACKEREARTH/01e648028ad911ea/Dataset/Train_images/TR_2.jpg'
-# path = 'helloworld.png'
+path = 'helloworld.png'
+# path = 'D:/Coding_wo_cp/OCR/t.png'
 image = cv2.imread(path)
 orig = image.copy() 
 orig1 = image.copy()          
@@ -33,10 +34,17 @@ for i in range(len(contours)) :
         new_contours.append(contours[i])
         # im1 = cv2.rectangle(orig1,(x-2,y-2),(x+w+2,y+h+2),(0,255,0),1)
 
-tolerance = 3
+# while True:
+#     cv2.imshow("i",im1)
+#     if cv2.waitKey(0) :
+#         break
+# cv2.destroyAllWindows()  
+# plt.imshow(im1)
+
 avgH = sum(h for _,_,_,h in boxes)/len(boxes)
 avgW = sum(h for _,_,w,_ in boxes)/len(boxes)
 
+print(len(boxes))
 boxes.sort(key= lambda x :(x[1]+x[3]))
 xySortedBoxes = []
 temp = []
@@ -56,11 +64,14 @@ for i in range(1,(len(boxes))) :
     # print(y+h)
 if len(temp)>0:
     xySortedBoxes.append(temp) 
+    
 
+tolerance = 10
 for lst in xySortedBoxes:
     for box in lst:     
         if box[3] < avgH-tolerance:
             lst.remove(box)
+
 
 c=1
 for i in range(len(xySortedBoxes)) :
@@ -71,28 +82,24 @@ for i in range(len(xySortedBoxes)) :
         # im1 = cv2.putText(im1,str(c), (x,y),cv2.FONT_HERSHEY_COMPLEX,1,[125])
         c+=1
 
-
-# while True:
-#     cv2.imshow("i",im1)
-#     if cv2.waitKey(0) :
-#         break
-# cv2.destroyAllWindows()  
-# plt.imshow(im1)
-
 mapp = pd.read_csv('emnist/emnist-balanced-mapping.txt',delimiter=' ', 
                    index_col=0,
                    header=None,
                    squeeze=True)
 model = tf.keras.models.load_model('D:/Coding_wo_cp/OCR/model-3/model_5.h5')
 
+text = ""
 f=0
 c=1
 for i in range(len(xySortedBoxes)) :
+    text+='\n'
     for j in range(len(xySortedBoxes[i])) :
 
         # plt.subplot(3,4,c)
         
         x,y,w,h=xySortedBoxes[i][j]
+        if j+1<len(xySortedBoxes[i]):
+            x1,y1,w1,h1=xySortedBoxes[i][j+1]
         im = orig[y-2:y+h+2,x-2:x+w+2]
         im = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
         im = np.pad(im,8,constant_values=255)
@@ -105,6 +112,11 @@ for i in range(len(xySortedBoxes)) :
         pred = model.predict(im)
         idx = np.argmax(pred,axis=1)
         value = chr(mapp[idx])
+        if x1-(x+w)<=25: 
+            text+=value
+        else :
+            text+=value
+            text+=' '
         # plt.title(value)
         imp = cv2.rectangle(orig1,(x-2,y-2),(x+w+2,y+h+2),(0,255,0),1)
         imp = cv2.putText(imp,str(value), (x,y),cv2.FONT_HERSHEY_COMPLEX,1,[125])
@@ -116,12 +128,16 @@ for i in range(len(xySortedBoxes)) :
 
     if f==1 :
         break    
-cv2.destroyAllWindows()         
+cv2.destroyAllWindows()      
+text = text.lower()   
+print(text)
 
-# while True:
-#     cv2.imshow("i",im1)
-#     if cv2.waitKey(0) :
-#         break
-# cv2.destroyAllWindows()
-
-
+from spellchecker import  SpellChecker
+spell = SpellChecker()
+mis = spell.unknown(text.split())
+for word in mis:
+    # Get the one `most likely` answer
+    print(spell.correction(word))
+    text =text.replace(word,spell.correction(word))
+    # Get a list of `likely` options
+    print(spell.candidates(word))
